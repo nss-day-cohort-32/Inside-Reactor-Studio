51da9665d1b66e45dc4a00710b41450ad0418345
Josh Webb <joshwebb42@gmail.com>
2019-05-28 15:49:08 -0500

    WE GOT DOM

--- src/components/Nutshell.js
+++ src/components/Nutshell.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ApplicationViews from './ApplicationViews'
+import NavBar from './nav/NavBar'
 
 class Nutshell extends Component {
   render() {
     return (
 
       <React.Fragment>
+        <NavBar />
         <ApplicationViews />
       </React.Fragment>
     );
