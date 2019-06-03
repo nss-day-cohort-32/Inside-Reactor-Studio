b8d628af0391beb555c1b15945a8b04d6ee5a640
Josh Webb <joshwebb42@gmail.com>
2019-06-03 10:07:16 -0500

    We have messages?

---
+++ src/modules/MessageManager.js
@@ -0,0 +1,36 @@
+const remoteURL = "http://localhost:5002"
+
+export default {
+  get(id) {
+    return fetch(`${remoteURL}/messages/${id}`).then(e => e.json())
+  },
+  getAll() {
+    return fetch(`${remoteURL}/messages?_sort=date&_order=desc`).then(e => e.json())
+  },
+  deleteMessage(id) {
+    return fetch(`${remoteURL}/messages/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      },
+    }).then(e => e.json())
+  },
+  post(message) {
+    return fetch(`${remoteURL}/messages`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(message)
+    }).then(e => e.json())
+  },
+  put(editedMessage) {
+    return fetch(`${remoteURL}/messages/${editedMessage.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(editedMessage)
+    }).then(data => data.json());
+  }
+}
